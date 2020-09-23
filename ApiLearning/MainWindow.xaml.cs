d4f04f9c1729694d4434e715204caf694dd9fc08
Joshua <joshual0821@gmail.com>
2020-09-23 18:16:07 -0500

    Add project files.

---
+++ ApiLearning/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ApiLearning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IGetWeatherInfo
+    {
+        /*
+         * Order of submission UriOneLocation + NAMEQUERY + CityName + APIKEY
+         */
+        public string CityName = "Katy";
+        const string APIKEY = "&appid=3330f05caa5758104b85b39b04d3efc1";
+        const string NAMEQUERY = "?q=";
+        const string UriOneLocation = "http://api.openweathermap.org/data/2.5/weather";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //sets up correct URL info to get data via api call
+        private void btnWeatherData_Click(object sender, RoutedEventArgs e)
+        {
+            string CompletedURL = WeatherDataRetrived.URLBuild(CityName, UriOneLocation, NAMEQUERY, APIKEY);
+            WeatherDataRetrived wInfo = JsonConvert.DeserializeObject<WeatherDataRetrived>(GetWeatherData(CompletedURL));
+
+            //Testing
+            WeatherDataBox.Text = wInfo.name;
+            WeatherDataBox.Text += "\n" + wInfo.main.temp;
+            WeatherDataBox.Text += "\n" + wInfo.wind.speed;
+
+            #region TestingTotalAmountOfPropertiesClassHas
+            //using reflection and linq to check retrived data (wi = WeatherInfo)
+            var dataobtained = from wi in Assembly.GetExecutingAssembly().GetTypes()
+                               where wi.GetCustomAttributes().Any( a => a is WDRetrivedAttribute)
+                               select wi;
+                               
+
+            foreach(Type WData in dataobtained)
+            {//scaning each item for all properties
+                foreach (var props in WData.GetProperties())
+                {
+                    Debug.WriteLine(props.Name);
+                }
+                
+            }
+            #endregion
+
+        }
+
+        public string GetWeatherData(string uri)
+        {//established http request and response. then return the string value
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ ApiLearning/WeatherDataRetrived.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiLearning
+{
+    [WDRetrived]
+    class WeatherDataRetrived
+    {
+        //building properties for deserializing json via newtonsoft.json
+        public List<Weatherinfo> weather { get; set; }
+        public string name { get; set; }
+        public MainTemps main { get; set; }
+        public WindInfo wind { get; set; }
+
+        //Able to retrive Id and base info of weather
+        public class Weatherinfo
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        //Able to retrive Tempertures
+        public class MainTemps
+        {
+            public float temp { get; set; }
+            public float feels_like { get; set; }
+            public float temp_min { get; set; }
+            public float temp_max { get; set; }
+            public float pressure { get; set; }
+            public float humidity { get; set; }
+        }
+
+        //Wind Information
+        public class WindInfo
+        {
+            public float speed { get; set; }
+            public float deg { get; set; }
+            public float gust { get; set; }
+        }
+
+
+        /// <summary>
+        /// Put the Uri together into a valid URL Address
+        /// </summary>
+        /// <param name="_CityName"></param>
+        /// <param name="_API_KEY"></param>
+        /// <returns></returns>
+        public static string URLBuild(string _CityName, string _UriOneLocation, string _NAMEQUERY, string _APIKEY)
+        {
+            //Building the new URL Address
+            StringBuilder BuiltURL = new StringBuilder(_UriOneLocation + _NAMEQUERY + _CityName + _APIKEY);
+
+            //building new URL address and returning it's value as string
+            return BuiltURL.ToString();
+
+        }
+    }
+
+
+    
+
+    [AttributeUsage(AttributeTargets.Class)]
+    class WDRetrivedAttribute : Attribute
+    {
+
+    }
+}
